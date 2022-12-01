8eb34f6bb400b3e077820227146b3069af7344c8
Tim van den Essen <tim3345@live.nl>
2022-12-01 06:08:51 +0100

    Add 2022 day 1

--- AoC.Puzzles.2022/Puzzles/Day01.cs
+++ AoC.Puzzles.2022/Puzzles/Day01.cs
@@ -4,20 +4,20 @@ using AoC.Common.Interfaces;
 namespace AoC.Puzzles._2022.Puzzles;
 
 [Puzzle(2022, 1, "Puzzle one")]
-public class Day01 : IPuzzle<int[]>
+public class Day01 : IPuzzle<int[][]>
 {
-    public int[] Parse(string inputText)
+    public int[][] Parse(string inputText)
     {
-        return inputText.Split(Environment.NewLine).Select(int.Parse).ToArray();
+        return inputText.Split(Environment.NewLine + Environment.NewLine).Select(x => x.Split(Environment.NewLine).Select(int.Parse).ToArray()).ToArray();
     }
 
-    public string Part1(int[] input)
+    public string Part1(int[][] input)
     {
-        return "Not implemented.";
+        return input.Select(x => x.Sum()).Max().ToString();
     }
 
-    public string Part2(int[] input)
+    public string Part2(int[][] input)
     {
-        return "Not implemented.";
+        return input.Select(x => x.Sum()).OrderDescending().Take(3).Sum().ToString();
     }
 }
