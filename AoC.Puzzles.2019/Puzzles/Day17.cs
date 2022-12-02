14a431914dfe7fe96e1edfd6461f5973b684eeb5
Tim van den Essen <tim.van.den.essen@wolfpackit.nl>
2022-12-02 08:17:38 +0100

    add more days

---
+++ AoC.Puzzles.2019/Puzzles/Day17.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using AoC.Common.Attributes;
+using AoC.Common.Interfaces;
+using AoC.Puzzles._2019.Shared;
+
+namespace AoC.Puzzles._2019.Puzzles;
+
+[Puzzle(2019, 17, "Repair drone")]
+public class Day17 : IPuzzle<long[]>
+{
+    public long[] Parse(string inputText)
+    {
+        return inputText.Split(',').Select(long.Parse).ToArray();
+    }
+
+    public string Part1(long[] input)
+    {
+        var computer = new Computer(input);
+
+        return "Not implemented";
+    }
+
+    public string Part2(long[] input)
+    {
+        var computer = new Computer(input);
+
+        return "Not implemented";
+    }
+}
---
+++ AoC.Puzzles.2019/Puzzles/Day19.cs
@@ -0,0 +1,23 @@
+using AoC.Common.Attributes;
+using AoC.Common.Interfaces;
+
+namespace AoC.Puzzles._2019.Puzzles;
+
+[Puzzle(2019, 19, "Bla bla")]
+public class Day19 : IPuzzle<long[]>
+{
+    public long[] Parse(string rawInput)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Part1(long[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Part2(long[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
